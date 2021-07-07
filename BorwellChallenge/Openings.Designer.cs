94c3934ddf958b89e8f1b8e5ebb645530bcf618b
Noah Barnett <52221065+NoahBarnett06@users.noreply.github.com>
2021-07-07 17:22:47 +0100

    Add files via upload

---
+++ BorwellChallenge/Openings.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace BorwellChallenge
+{
+    partial class frmOpenings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstOpenings = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.txtOpeningWidth = new System.Windows.Forms.TextBox();
+            this.txtOpeningHeight = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotalOpenings = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(126, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(323, 74);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Paint Calculator -\r\nDoors and Windows";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lstOpenings
+            // 
+            this.lstOpenings.FormattingEnabled = true;
+            this.lstOpenings.ItemHeight = 20;
+            this.lstOpenings.Location = new System.Drawing.Point(209, 122);
+            this.lstOpenings.Name = "lstOpenings";
+            this.lstOpenings.Size = new System.Drawing.Size(367, 144);
+            this.lstOpenings.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.White;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.Black;
+            this.btnAdd.Location = new System.Drawing.Point(184, 290);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(220, 57);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add Door or Window";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.White;
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.Black;
+            this.btnSubmit.Location = new System.Drawing.Point(406, 290);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(170, 57);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.White;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.Color.Black;
+            this.btnBack.Location = new System.Drawing.Point(12, 290);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(170, 57);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.Text = "Previous Page";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // txtOpeningWidth
+            // 
+            this.txtOpeningWidth.Location = new System.Drawing.Point(83, 206);
+            this.txtOpeningWidth.MaxLength = 5;
+            this.txtOpeningWidth.Name = "txtOpeningWidth";
+            this.txtOpeningWidth.Size = new System.Drawing.Size(99, 26);
+            this.txtOpeningWidth.TabIndex = 2;
+            // 
+            // txtOpeningHeight
+            // 
+            this.txtOpeningHeight.Location = new System.Drawing.Point(83, 158);
+            this.txtOpeningHeight.MaxLength = 5;
+            this.txtOpeningHeight.Name = "txtOpeningHeight";
+            this.txtOpeningHeight.Size = new System.Drawing.Size(99, 26);
+            this.txtOpeningHeight.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Width:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 20);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Height:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(117, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(344, 20);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "All measurements in metres to 2 decimal places";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 20);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "Add a door or window:";
+            // 
+            // lblTotalOpenings
+            // 
+            this.lblTotalOpenings.AutoSize = true;
+            this.lblTotalOpenings.Location = new System.Drawing.Point(17, 246);
+            this.lblTotalOpenings.Name = "lblTotalOpenings";
+            this.lblTotalOpenings.Size = new System.Drawing.Size(117, 20);
+            this.lblTotalOpenings.TabIndex = 39;
+            this.lblTotalOpenings.Text = "Total Area: ???";
+            // 
+            // frmOpenings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 374);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblTotalOpenings);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtOpeningWidth);
+            this.Controls.Add(this.txtOpeningHeight);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstOpenings);
+            this.Controls.Add(this.label4);
+            this.Name = "frmOpenings";
+            this.Text = "Doors and Windows";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstOpenings;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.TextBox txtOpeningWidth;
+        private System.Windows.Forms.TextBox txtOpeningHeight;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotalOpenings;
+    }
+}
